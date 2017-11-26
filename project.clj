80479da88f44d090aced35e90dc3b5775363ff9f
Tony Tang <tonyt@ucalgary.ca>
2017-11-26 22:24:27 +0800

    FIX: time-based update really works now! woo

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
     [org.clojure/data.json "0.2.6"]
     [clj-http "3.7.0"]
     [clojure-lanterna "0.9.7"]
-    [clojure.java-time "0.3.0"]
+    ;[clojure.java-time "0.3.0"]
+    [clj-time "0.14.2"]
     ]
   :main ^:skip-aot nhl-watcher-app.core
   :target-path "target/%s"
